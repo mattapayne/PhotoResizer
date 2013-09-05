2a93bc0cc55604cbddb1a4b6fce22b36ee7e83bc
Matt Payne <paynmatt@gmail.com>
2013-09-05 15:21:24 -0700

    First commit

---
+++ PhotoResizer/Main.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PhotoResizer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fbd
+            // 
+            this.fbd.Description = "Please select the folder containing your pictures";
+            this.fbd.ShowNewFolderButton = false;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(18, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(134, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(242, 146);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "Ok";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(161, 146);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(18, 118);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(299, 22);
+            this.txtDirectory.TabIndex = 3;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(18, 73);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 22);
+            this.txtWidth.TabIndex = 4;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(127, 75);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 22);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(15, 53);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(75, 17);
+            this.lblWidth.TabIndex = 6;
+            this.lblWidth.Text = "New Width";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(124, 55);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(80, 17);
+            this.lblHeight.TabIndex = 7;
+            this.lblHeight.Text = "New Height";
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(15, 98);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(153, 17);
+            this.lblDirectory.TabIndex = 8;
+            this.lblDirectory.Text = "Output Directory Name";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 171);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Photo Resizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblDirectory;
+    }
+}
+
---
+++ PhotoResizer/Main.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using PhotoResizer.Properties;
+
+namespace PhotoResizer
+{
+    public partial class Main : Form
+    {
+        private string _selectedPath;
+        private const int DefaultImgWidth = 640;
+        private const int DefaultImgHeight = 360;
+        private const string FileExtension = "*.jpg";
+        private const string DefaultResizedDirectoryName = "resized";
+
+        public Main()
+        {
+            _selectedPath = String.Empty;
+            InitializeComponent();
+            txtDirectory.Text = DefaultResizedDirectoryName;
+            txtHeight.Text = DefaultImgHeight.ToString();
+            txtWidth.Text = DefaultImgWidth.ToString();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            var result = fbd.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                _selectedPath = fbd.SelectedPath;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(_selectedPath))
+            {
+                MessageBox.Show(Resources.NoFolderSelectedErrorMessage, 
+                    Resources.NoFolderSelectedErrorTitle);
+
+                return;
+            }
+
+            ProcessImages();
+        }
+
+        private void ProcessImages()
+        {
+            var folder = new DirectoryInfo(_selectedPath);
+
+            if(!folder.Exists)
+            {
+                MessageBox.Show(Resources.SelectedFolderDoesNotExistErrorMessage, 
+                    Resources.SelectedFolderDoesNotExistErrorTitle);
+                return;
+            }
+
+            var files = folder.GetFiles(FileExtension);
+
+            if(!files.Any())
+            {
+                MessageBox.Show(Resources.NoPicturesInSelectedFolderErrorMessage,
+                    Resources.NoPicturesInSelectedFolderErrorTitle);
+                return;
+            }
+
+            ProcessImages(files);
+            MessageBox.Show(Resources.SuccessfullyResizedImagesMessage,
+                Resources.ResizedImagesSuccessTitle);
+        }
+
+        private void ProcessImages(IEnumerable<FileInfo> files)
+        {
+            var resizedImageFolder = Path.Combine(_selectedPath, ResizedDirectoryName);
+
+            if(Directory.Exists(resizedImageFolder))
+            {
+                Directory.Delete(resizedImageFolder, true);
+            }
+
+            Directory.CreateDirectory(resizedImageFolder);
+
+            var jpegEncoder = GetEncoder(ImageFormat.Jpeg);
+            var qualityEncoder = Encoder.Quality;
+            var encoderParams = new EncoderParameters(1);
+            var encoderParam = new EncoderParameter(qualityEncoder, 90L);
+            encoderParams.Param[0] = encoderParam;
+
+            foreach(var imageFile in files)
+            {
+                var image = Image.FromFile(imageFile.FullName);
+                var imageName = imageFile.Name;
+
+                using(var resizedImage = new Bitmap(image, new Size(ImgWidth, ImgHeight)))
+                {
+                    resizedImage.Save(Path.Combine(resizedImageFolder, imageName), jpegEncoder, encoderParams);     
+                }
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageDecoders();
+            return codecs.FirstOrDefault(codec => codec.FormatID == format.Guid);
+        }
+
+        private int ImgHeight
+        {
+            get
+            {
+                if(!String.IsNullOrEmpty(txtHeight.Text))
+                {
+                    int height;
+
+                    if(Int32.TryParse(txtHeight.Text, out height))
+                    {
+                        return height;
+                    }
+                }
+
+                return DefaultImgHeight;
+            }
+        }
+
+        private int ImgWidth
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(txtWidth.Text))
+                {
+                    int width;
+
+                    if (Int32.TryParse(txtWidth.Text, out width))
+                    {
+                        return width;
+                    }
+                }
+
+                return DefaultImgWidth;
+            }
+        }
+
+        private string ResizedDirectoryName
+        {
+            get
+            {
+                return !String.IsNullOrEmpty(txtDirectory.Text) ? txtDirectory.Text : DefaultResizedDirectoryName;
+            }
+        }
+    }
+}
