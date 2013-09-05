0d2f8db4dd8af5f1415fd29dd501d97f44a3c953
Matt Payne <paynmatt@gmail.com>
2013-09-05 15:46:28 -0700

    Add install project

--- PhotoResizer/Properties/AssemblyInfo.cs
+++ PhotoResizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TamarasPictureResizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PhotoResizer")]
+[assembly: AssemblyDescription("Resizes Photos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TamarasPictureResizer")]
+[assembly: AssemblyCompany("Matt Payne Consulting")]
+[assembly: AssemblyProduct("PhotoResizer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
